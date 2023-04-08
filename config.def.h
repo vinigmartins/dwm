a6150d7503234772d3d45190a90166aa5676768d
vinicius martins <vinicius.gmartins3@gmail.com>
2023-04-08 01:59:18 -0300

    style: change fonts & keys

--- config.def.h
+++ config.def.h
@@ -11,22 +11,24 @@ static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display 
 static const int showsystray        = 1;        /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const int user_bh            = 4;        /* 2 is the default spacing around the bar's font */
-static const char *fonts[]          = { "Iosevka:Bold:size=12", "Symbols Nerd Font:Semibold:size=12" };
-static const char dmenufont[]       = "Iosevka:Bold:size=12";
+static const int user_bh            = 0;        /* 2 is the default spacing around the bar's font */
+/* static const char *fonts[]          = { "Iosevka Term Nerd Font Complete:Mono:Medium:pixelsize=12:antialias=true:autohint=true" }; */
+static const char *fonts[]          = { "Iosevka Term Nerd Font Complete:Medium:size=12" };
+static const char dmenufont[]       = "monospace: size = 14";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
 static const char col_cyan[]        = "#005577";
+static const char col_red[]         = "#ff0000";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeSel]  = { col_red, col_gray1,  col_cyan  },
 };
 
 /* tagging */
-static const char *tags[] = { "\ue795", "\uf269", "\ufb6e", "\uf1bc", "\uf11b", "\uf79f", "\uf79f" };
+static const char *tags[] = { "", "\uf269", "\ufb6e", "\uf1bc", "\uf11b", "\uf79f", "\uf79f" };
 
 static const Rule rules[] = {
 	/* xprop(1):
@@ -35,7 +37,8 @@ static const Rule rules[] = {
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
 	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+	{ "Brave",    NULL,       NULL,       1 << 1,       0,           -1 },
+	{ "Discord",  NULL,       NULL,       1 << 2,       0,           -1 },
 };
 
 /* layout(s) */
@@ -66,12 +69,15 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
+static const char *fmcmd[]  = { "thunar", NULL };
 static const char maimcmd[]  = { "maim -s | xclip -selection clipboard -t image/png" };
 
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
-	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
+	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
+	{ MODKEY,                       XK_e,      spawn,          {.v = fmcmd} },
+	{ Mod4Mask,                     XK_Return, zoom,           {0} },
     { 0,                            XK_Print,  spawn,          SHCMD(maimcmd) },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
@@ -80,14 +86,12 @@ static const Key keys[] = {
 	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
 	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
 	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
-	{ MODKEY,                       XK_Return, zoom,           {0} },
 	{ MODKEY,                       XK_Tab,    view,           {0} },
-	{ MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
+	{ MODKEY,                       XK_q,      killclient,     {0} },
 	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
 	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
 	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
-	{ MODKEY,                       XK_space,  setlayout,      {0} },
-	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
+	{ MODKEY,                       XK_space,  togglefloating, {0} },
 	{ MODKEY|ShiftMask,             XK_f,      togglefullscr,  {0} },
 	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
 	{ MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
